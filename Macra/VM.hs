d2f089a21a5f0ada9852a1774da6c9db426ab7fc
pasberth <pasberth@gmail.com>
2012-10-16 21:57:08 +0900

    関数が宣言されたあとにdefineされた変数も参照可能にする。Envだと変更されたら参照できないのでEnvRef作った

--- Macra/VM.hs
+++ Macra/VM.hs
@@ -1,19 +1,21 @@
 module Macra.VM (Value(..), Identifier(..), Inst(..), vm) where
 
+import qualified Data.Unique as U
 import qualified Data.Map as M
 import qualified Control.Monad.State as S
 
 data Value = Double Double
            | Char Char
            | List [Value]
-           | Closure Identifier Inst Env
+           | Closure Identifier Inst EnvRef
            deriving (Eq, Ord)
 
 instance Show Value where
   show (Char c) = [c]
   show (Double i) = show i
   show (List xs) = concat ["(", concat (map (\x -> (show x) ++ ";") xs), ")"]
-  show (Closure var body e) = concat [show var, show body, show e]
+  show (Closure var body e) = concat [show var, show body]
+
 data Identifier = Sym String | Nil deriving (Show, Eq, Ord)
 
 data Inst = FrameInst  Inst       Inst      --hasnext
@@ -32,89 +34,114 @@ data Inst = FrameInst  Inst       Inst      --hasnext
 data VM = VM {
      vmAcc :: Value
    , vmInst :: Inst
-   , vmEnv :: Env
+   , vmEnvRef :: EnvRef
    , vmRib :: Rib
    , vmStack :: Stack
-     } deriving (Show)
+   , vmEnvMem :: EnvMem
+     }
+
+instance Show VM where
+  show (VM a x envRef r s envMem) = concat ["A: ", show a, "\n",
+                                            "X: ", show x, "\n",
+                                            "E: ", (case M.lookup envRef envMem of
+                                                     Just e -> show e
+                                                     Nothing -> "Environment Reference Error"), "\n",
+                                            "R: ", show r, "\n",
+                                            "S: ", "TODO", "\n"]
+
 
 type Env = M.Map Identifier Value
 type Rib = [Value]
-type Stack = [(Inst, Env, Rib)]
+type Stack = [(Inst, EnvRef, Rib)]
 type VMCommand = S.StateT VM IO ()
+
+type EnvRef = U.Unique
+type EnvMem = M.Map EnvRef Env
 
 nil :: Value
 nil = List []
-initialVM = VM {
-          vmAcc = nil
-        , vmInst = HaltInst
-        , vmEnv = M.fromList []
-        , vmRib = []
-        , vmStack = []
-          }
 
 vm :: Inst -> IO ()
-vm inst = S.evalStateT vm' initialVM { vmInst = inst }
+vm inst = do
+        envRef <- U.newUnique
+        S.evalStateT vm' VM {
+                         vmAcc = nil
+                       , vmInst = inst
+                       , vmEnvRef = envRef
+                       , vmRib = []
+                       , vmStack = []
+                       , vmEnvMem = M.fromList [(envRef, M.fromList [])]
+                         }
 
 vm' :: VMCommand
 vm' = do
   vmState <- S.get
   case vmState of
-    VM a HaltInst e r s -> do
+    VM a HaltInst e r s _ -> do
       S.liftIO $ print vmState
       return ()
-    VM a (ConstExpr val nxt) e r s -> do
+    VM a (ConstExpr val nxt) e r s _ -> do
       S.put vmState {
             vmAcc = val
           , vmInst = nxt
             }
       vm'
-    VM a (PrintInst nxt) e r s -> do
+    VM a (PrintInst nxt) e r s _ -> do
       S.liftIO $ print a
       S.put vmState {
             vmInst = nxt
             }
       vm'
-    VM a (ReferInst id nxt) e r s -> do
-      case M.lookup id e of
-        Just v -> do
+    VM a (ReferInst id nxt) envRef r s envMem -> do
+      case M.lookup envRef envMem of
+        Just e -> do
+          case M.lookup id e of
+            Just v -> do
+              S.put vmState {
+                    vmAcc = v
+                  , vmInst = nxt
+                    }
+              vm'
+            Nothing -> do
+              S.liftIO $ do
+                putStr $ concat ["unbound variable: `", show id, "'"]
+              return ()
+    VM a (DefineInst id nxt) envRef r s mem -> do
+      case M.lookup envRef mem of
+        Just e -> do
           S.put vmState {
-                vmAcc = v
+                vmEnvMem = M.insert envRef (M.insert id a e) mem
               , vmInst = nxt
                 }
           vm'
-        Nothing -> do
-          S.liftIO $ do
-            putStr $ concat ["unbound variable: `", show id, "'"]
           return ()
-    VM a (DefineInst id nxt) e r s -> do
+    VM a (FrameInst ret nxt) envRef r s _ -> do
       S.put vmState {
-            vmEnv = M.insert id a e
+            vmStack = (ret, envRef, r):s
           , vmInst = nxt
             }
       vm'
-    VM a (FrameInst ret nxt) e r s -> do
-      S.put vmState {
-            vmStack = (ret, e, r):s
-          , vmInst = nxt
-            }
-      vm'
-    VM a (ArgInst nxt) e r s -> do
+    VM a (ArgInst nxt) e r s _ -> do
       S.put vmState {
             vmRib = a:r
           , vmInst = nxt
             }
       vm'
-    VM a ApplyInst e (val:r) s -> do
+    VM a ApplyInst envRef (val:r) s _ -> do
       case a of
-        (Closure (Sym var) body ce) -> do
+        (Closure (Sym var) body envRef) -> do
+          case M.lookup envRef (vmEnvMem vmState) of
+            Just ce -> do
+              S.put vmState {
+                    vmEnvMem = M.insert envRef (M.insert (Sym var) val ce) (vmEnvMem vmState)
+                  , vmInst = body
+                    }
+              vm'
+            Nothing -> do
+              return ()
+        (Closure Nil body envRef) -> do
           S.put vmState {
-                vmEnv = M.insert (Sym var) val ce
-              , vmInst = body
-                }
-          vm'
-        (Closure Nil body ce) -> do
-          S.put vmState {
-                vmEnv = ce
+                vmEnvRef = envRef
               , vmInst = body
                 }
           vm'
@@ -122,21 +149,21 @@ vm' = do
           S.liftIO $ do
             putStr $ concat ["invalid application: ", show a]
           return ()
-    VM a ReturnInst _ _ ((ret, e, r):s) -> do
-      S.put vmState {
-            vmInst = ret
-          , vmEnv = e
-          , vmRib = r
-          , vmStack = s
-            }
-      vm'
-    VM a ReturnInst e r [] -> do
+    VM a ReturnInst _ _ ((ret, envRef, r):s) _ -> do
+        S.put vmState {
+              vmInst = ret
+            , vmEnvRef = envRef
+            , vmRib = r
+            , vmStack = s
+              }
+        vm'
+    VM a ReturnInst e r [] _ -> do
       S.liftIO $ do
         putStr $ concat ["stack is empty"]
       return ()
-    VM a (CloseInst var body nxt) e r s -> do
+    VM a (CloseInst var body nxt) envRef r s _ -> do
       S.put vmState {
-            vmAcc = Closure var body e
+            vmAcc = Closure var body envRef
           , vmInst = nxt
             }
       vm'
