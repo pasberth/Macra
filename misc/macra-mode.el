857dd15a73b99c5ec6f85e3fcc0a46d9f88fd3d8
pasberth <pasberth@gmail.com>
2012-11-08 02:59:07 +0900

    macra-mode.el $ cp misc/macra-mode.el ~/.emacs.d/lib $ echo "(require 'macra-mode)" >> ~/.emacs.d/init.el

---
+++ misc/macra-mode.el
@@ -0,0 +1,35 @@
+(eval-when-compile (require 'cl))
+
+(defvar macra-mode-map
+  (let ((map (make-sparse-keymap)))
+    map)
+  "Keymap used in Macra mode.")
+
+(defconst macra-font-lock-keywords
+  (list
+    (concat
+       (regexp-opt
+         '( "!if"
+            "!lambda"
+            "!define"
+            "!return"
+            "!funcall"
+            "!print")
+         t))))
+;;;###autoload
+(defun macra-mode ()
+  "test macra mode"
+  (interactive)
+  (kill-all-local-variables)
+  (use-local-map macra-mode-map)
+  (set (make-local-variable 'font-lock-defaults)
+          '((macra-font-lock-keywords) nil nil))
+  (set (make-local-variable 'font-lock-keywords)
+        macra-font-lock-keywords)
+  (setq mode-name "Macra"))
+;;;###autoload
+(add-to-list 'auto-mode-alist (cons (purecopy "\\.macra\\'") 'macra-mode))
+
+(provide 'macra-mode)
+
+;;; macra-mode.el ends here
