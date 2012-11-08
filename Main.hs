23b270626f440e29b454d367242215a517e732aa
pasberth <pasberth@gmail.com>
2012-11-08 20:58:08 +0900

    --nodes とかでファイルから読んだ方が便利

--- Main.hs
+++ Main.hs
@@ -9,19 +9,25 @@ import Macra.Compiler
 main = do
   args <- getArgs
   case args of
-    "--nodes":str:xs -> case parse "(fname)" str of
-                             Left x -> print x
-                             Right x -> print x
-    "--insts":str:xs ->  case parse "(fname)" str of
-                             Left x -> print x
-                             Right x -> print $ compile (macroDefine x)
-                                                        (signatureDefine x)
-                                                        x
-    "--eval":str:xs ->  case parse "(fname)" str of
-                             Left x -> print x
-                             Right x -> vm (compile (macroDefine x)
-                                                    (signatureDefine x)
-                                                    x)
+    "--nodes":fname:xs -> do
+      str <- readFile fname
+      case parse fname str of
+           Left x -> print x
+           Right x -> print x
+    "--insts":fname:xs ->  do
+      str <- readFile fname
+      case parse fname str of
+           Left x -> print x
+           Right x -> print $ compile (macroDefine x)
+                                      (signatureDefine x)
+                                      x
+    "--eval":fname:xs -> do
+      str <- readFile fname
+      case parse fname str of
+           Left x -> print x
+           Right x -> vm (compile (macroDefine x)
+                                  (signatureDefine x)
+                                  x)
     path:xs -> do
             str <- readFile path
             case parse path str of
