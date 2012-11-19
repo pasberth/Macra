03473a227409d4b455e41ebc62fc310e3153315a
pasberth <pasberth@gmail.com>
2012-11-19 23:07:43 +0900

    さらにテスト追加

--- spec/ParserSpec.hs
+++ spec/ParserSpec.hs
@@ -12,6 +12,8 @@ main = hspec spec
 q = SymNode
 c = CharNode
 n = NumNode
+nil = NilNode
+cons = ConsNode
 
 cmpNode :: String -> Node -> Expectation
 cmpNode program node =
@@ -44,17 +46,38 @@ spec = do
       it "should parse 12.3" $
         cmpNode "12.3" (n 12.3)
 
-      it "should raise ParseError when parsing to 00" $
+      it "should raise ParseError when parsing 00" $
+        -- 00 とかをエラーにする
         pending "hspec で ParseError を投げたとき pass するようなテストの書き方がわからない"
 
-      it "should raise ParseError when parsing to 00.0" $
+      it "should raise ParseError when parsing 00.0" $
         pending "hspec で ParseError を投げたとき pass するようなテストの書き方がわからない"
 
-      it "should raise ParseError when parsing to 0..0" $
+      it "should raise ParseError when parsing 0..0" $
         pending "hspec で ParseError を投げたとき pass するようなテストの書き方がわからない"
 
-      it "should raise ParseError when parsing to 0." $
+      it "should raise ParseError when parsing 0." $
         pending "hspec で ParseError を投げたとき pass するようなテストの書き方がわからない"
 
-      it "should raise ParseError when parsing to .0" $
+      it "should raise ParseError when parsing .0" $
         pending "hspec で ParseError を投げたとき pass するようなテストの書き方がわからない"
+
+
+    describe "Char" $ do
+
+      it "prefix is $'" $ do
+        cmpNode "$'c" (c 'c')
+
+      it "can contain Japanese" $ do
+        cmpNode "$'あ" (c 'あ')
+
+      it "should include spaces in between character literal and next token." $ do
+        -- $'ab とかをエラーにする
+        pending "hspec で ParseError を投げたとき pass するようなテストの書き方がわからない"
+
+    describe "String" $ do
+      it "文字列は文字を cons したものを返す" $ do
+        cmpNode "\"hoge\"" (cons (c 'h') (cons (c 'o') (cons (c 'g') (cons (c 'e') nil))))
+
+      it "日本語は1文字として扱う" $ do
+        cmpNode "\"日本語\"" (cons (c '日') (cons (c '本') (cons (c '語') nil)))
