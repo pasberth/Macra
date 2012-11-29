43cfe1dbf007b3b35e0344aa709c7d49e872882c
pasberth <pasberth@gmail.com>
2012-11-29 21:41:15 +0900

    機械生成されたコメントを削除

--- misc/macra.rb
+++ misc/macra.rb
@@ -1,28 +1,20 @@
 require 'formula'
 
-# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Macra < Formula
   homepage ''
   url "https://nodeload.github.com/pasberth/Macra/tar.gz/Formula4Homebrew"
   version '0.0.1'
   sha1 '65f88be9d17b43848d6e9ee79fc85b42e738941b'
 
-  depends_on :autoconf # if build.head?
+  depends_on :autoconf
   depends_on 'ghc'
-  # depends_on 'cmake' => :build
-  # depends_on :x11 # if your formula requires any X11/XQuartz components
 
   def install
-    # ENV.j1  # if your formula's build system can't parallelize
-    system "autoconf" # if build.head?
+    system "autoconf"
 
-    system "./configure", # "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "make compile"
-    system "make install" # if this fails, try separate make/make install steps
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "compile"
+    system "make", "install"
     ENV.prepend 'PATH', prefix/'bin', ':'
   end
 
