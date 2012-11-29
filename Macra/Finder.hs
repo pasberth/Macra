3d9938847d4109f6afce9f58e1853841102f11e0
pasberth <pasberth@gmail.com>
2012-11-30 01:09:29 +0900

    fix

--- Macra/Finder.hs
+++ Macra/Finder.hs
@@ -8,16 +8,19 @@ import Data.List.Split -- $ cabal install split
 
 type MacraLib = [FilePath]
 
+-- MACRALIB をコロン区切りで返す
 getMacraLib :: IO MacraLib
 getMacraLib = do {
   str <- getEnv "MACRALIB"
 ; return $ splitOn ":" str
 } `catch` (\_ -> return [])
 
-doesLibExist :: FilePath -> MacraLib -> IO Bool
-doesLibExist fname [] = return False
-doesLibExist fname (path:paths) = do
-  isExist <- doesFileExist (joinPath [path, fname])
-  if isExist
-    then return True
-    else doesLibExist fname paths
+-- MACRALIB にファイルが存在するなら真
+doesLibExist :: FilePath -> IO Bool
+doesLibExist fname = getMacraLib >>= doesLibExist' fname
+  where  doesLibExist' fname [] = return False
+         doesLibExist' fname (path:paths) = do
+           isExist <- doesFileExist (joinPath [path, fname])
+           if isExist
+             then return True
+             else doesLibExist' fname paths
