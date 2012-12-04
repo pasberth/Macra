b95a5028be87e003c1cee44a246b6cbeac0a96f4
pasberth <pasberth@gmail.com>
2012-12-04 18:17:34 +0900

    macri とりあえず作った。 今の Macra には VM から env 取り出す方法がない

--- Macri.hs
+++ Macri.hs
@@ -1,6 +1,7 @@
 #! /usr/bin/env runhaskell
 module Main where
 
+import System.IO
 import qualified Data.Map as M
 import Text.ParserCombinators.Parsec
 import Macra.Parser
@@ -14,21 +15,38 @@ macri :: MacroMap -> IO ()
 macri mm = do
 
   putStr prompt
+  hFlush stdout
   input <- getLine
   case parse runTimeExpr "(macri)" input of
     -- ランタイムのコードとして valid ならランタイムのコード
     Right node ->
       case compile mm node of
-        Left err -> print err >> macri mm
+        Left err -> do
+          print err
+          putStr "\n"
+          hFlush stdout
+          macri mm
         -- 今は vm が終了した後、 VM の状態を得る方法がない
-        Right inst -> vm inst >> macri mm
+        Right inst -> do
+          vm inst
+          putStr "\n"
+          hFlush stdout
+          macri mm
     -- そうでなければコンパイル時のコード
     Left _ ->
       case parse compileTimeExpr "(macri)" input of
-        Left err -> print err >> macri mm
+        Left err -> do
+          print err
+          putStr "\n"
+          hFlush stdout
+          macri mm
         Right cnode -> do
           r <- mkMacroMap cnode
           case r of
             Right mm' -> macri (mm' `M.union` mm)
-            Left err -> print err >> macri mm
+            Left err -> do
+              print err
+              putStr "\n"
+              hFlush stdout
+              macri mm
 main = macri M.empty
